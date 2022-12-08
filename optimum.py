cdb56aa78c1a539b70c423ecd8caf85cb4fa7712
sotiria-stef <51448676+sotiria-stef@users.noreply.github.com>
2022-12-08 16:46:56 +0200

    Update optimum.py

--- optimum.py
+++ optimum.py
@@ -17,6 +17,65 @@ import utilities
 
 PROJECT_DIR = os.getcwd()
 
+class RCJacketInput():
+    ## Core Properties
+    # diameter of circular column
+    d_init = 1.6
+    # Longitutinal Reinforcement Ratio    
+    rhol_init = 0.008
+    # Transverse Reinforcement Ratio
+    rhow_init = 0.0075
+    # Concrete Compressive Strength
+    fc_init = 24
+    # fy of core steel
+    fy_core = 440    
+    
+    
+    ## RC Jacket Properties
+    # fc of jacket concrete
+    fc_jack = 38
+
+    # fy of jacket steel
+    fy_jack = 550
+
+    # diameter of longitudinal reinforcement of jacket
+    dl_jack = 0.022
+    # diameter of transverse reinforcement of jacket
+    ds_jack = 0.016
+    lb, ub = JacketBounds(d_init)
+    # Cost and CO2 emission
+    cost = [1500, 1100, 1100]
+    co2 = [0.12, 0.684, 5]
+    # Weights from BWM
+    w = [0.45, 0.045, 0.23, 0.17, 0.1]
+
+class FRPInput():
+    ## Core Properties
+    # Diameter
+    d_init = 1.6
+    # Concrete Compressive Strength (MPa)
+    fc_init = 24
+    # Transverse reinforcement ratio of the core
+    rhow_init =0.0075
+    # Concrete Module of Elasticity
+    Ecore = 29
+
+    
+    ## FRP Jacket Properties
+    # FRP thickness
+    t_frp=[0.169/1000, 0.333/1000]
+    # FRP Initial Stiffness
+    Efrp=[210, 225]
+    # FRP  Tensile Strength
+    fjfrp=[3000, 3500]
+    # Cost and C02 emissions
+    cost=[400, 600]
+    co2= [0.12,0.684,5]
+    # Weights from BWM
+    w=[0.56, 0.052, 0.164, 0.2196]
+
+
+
 
 def out_to_ndarray(out):
     for key, val in out.items():
@@ -150,38 +209,16 @@ def JacketBounds(d_init):
     return lb, ub
 
 
-def JacketObj():
+def JacketObj(Args):
     intVars = [1, 2, 3]
     nvars = 3
     goal = 1.1
     goal2 = 1.05
-    # fc of core concrete
-    ######################################################
-    fc_init = 24
-    # fc of jacket concrete
-    fc_jack = 38
-    # fy of core steel
-    fy_core = 440
-    # fy of jacket steel
-    fy_jack = 550
-    # diameter of circular column
-    d_init = 1.6
-    # diameter of longitudinal reinforcement of jacket
-    dl_jack = 0.022
-    # diameter of transverse reinforcement of jacket
-    ds_jack = 0.016
-    lb, ub = JacketBounds(d_init)
-    rhol_init = 0.008
-    rhow_init = 0.0075
-      ######################################################
 
-    Alunit_jack = math.pi * (dl_jack ** 2) / 4
-    Asunit_jack = math.pi * (ds_jack ** 2) / 4
+    lb, ub = JacketBounds(Args.d_init)
 
-      ######################################################
-    cost = [1500, 1100, 1100]
-    co2 = [0.12, 0.684, 5]
-      ######################################################
+    Alunit_jack = math.pi * (Args.dl_jack ** 2) / 4
+    Asunit_jack = math.pi * (Args.ds_jack ** 2) / 4
 
     beta0 = [0.566, 1.167, 0.802, 1.094, -4.213]
     beta1 = [0.059, -0.353, -0.212, -0.274, 4.610]
@@ -191,27 +228,27 @@ def JacketObj():
     beta5 = [+0.399, +0.164, +0.528, +0.388, 0.253]
 
     obj = [
-        lambda x: (cost[0] * 3.14159 * (2 * 0.02 * x[0] * d_init + (0.02 * x[0]) ** 2) / 4 + cost[1] * (
-                Alunit_jack * x[1]) * 7.850 + cost[2] * (Asunit_jack / (0.025 * x[2])) * 3.14159 * (
-                           d_init + 0.02 * x[0]) * 7.850) / 1000,
+        lambda x: (Args.cost[0] * 3.14159 * (2 * 0.02 * x[0] * Args.d_init + (0.02 * x[0]) ** 2) / 4 + Args.cost[1] * (
+                Alunit_jack * x[1]) * 7.850 + Args.cost[2] * (Asunit_jack / (0.025 * x[2])) * 3.14159 * (
+                           Args.d_init + 0.02 * x[0]) * 7.850) / 1000,
 
-        lambda x: -(beta0[1] + beta4[1] * fc_jack / fc_init + beta5[1] * fy_jack / fy_core + beta1[1] * (
-                d_init + 0.02 * 2 * x[0]) / d_init + beta2[0] * (
-                            Alunit_jack * x[0] / (d_init + 0.02 * 2 * x[0]) ** 2 / rhol_init) + beta3[1] * (
-                            4 * Asunit_jack / (0.025 * x[2] * (d_init - 0.05 + 0.02 * x[0]))) / rhow_init) /
-                  (beta0[0] + beta4[0] * fc_jack / fc_init + beta5[0] * fy_jack / fy_core + beta1[0] * (
-                          d_init + 0.02 * 2 * x[0]) / d_init + beta2[0] * (
-                           Alunit_jack * x[1] / (d_init + 0.02 * 2 * x[0]) ** 2 / rhol_init) + beta3[0] * (
-                           4 * Asunit_jack / (0.025 * x[2] * (d_init - 0.05 + 0.02 * x[0]))) / rhow_init),
+        lambda x: -(beta0[1] + beta4[1] * Args.fc_jack / Args.fc_init + beta5[1] * Args.fy_jack / Args.fy_core + beta1[1] * (
+                Args.d_init + 0.02 * 2 * x[0]) / Args.d_init + beta2[0] * (
+                            Alunit_jack * x[0] / (Args.d_init + 0.02 * 2 * x[0]) ** 2 / Args.rhol_init) + beta3[1] * (
+                            4 * Asunit_jack / (0.025 * x[2] * (d_init - 0.05 + 0.02 * x[0]))) / Args.rhow_init) /
+                  (beta0[0] + beta4[0] * Args.fc_jack / Args.fc_init + beta5[0] * Args.fy_jack / Args.fy_core + beta1[0] * (
+                          Args.d_init + 0.02 * 2 * x[0]) / Args.d_init + beta2[0] * (
+                           Alunit_jack * x[1] / (Args.d_init + 0.02 * 2 * x[0]) ** 2 / Args.rhol_init) + beta3[0] * (
+                           4 * Asunit_jack / (0.025 * x[2] * (Args.d_init - 0.05 + 0.02 * x[0]))) / Args.rhow_init),
 
-        lambda x: -(beta0[2] + beta4[2] * fc_jack / fc_init + beta5[2] * fy_jack / fy_core + beta1[2] * (
-                d_init + 0.02 * 2 * x[0]) / d_init + beta2[2] * (
-                            Alunit_jack * x[1] / (d_init + 0.02 * 2 * x[0]) ** 2 / rhol_init) + beta3[2] * (
-                            4 * Asunit_jack / (0.025 * x[2] * (d_init - 0.05 + 0.02 * x[0]))) / rhow_init) /
-                  (beta0[0] + beta4[0] * fc_jack / fc_init + beta5[0] * fy_jack / fy_core + beta1[0] * (
-                          d_init + 0.02 * 2 * x[0]) / d_init + beta2[0] * (
-                           Alunit_jack * x[1] / (d_init + 0.02 * 2 * x[0]) ** 2 / rhol_init) + beta3[0] * (
-                           4 * Asunit_jack / (0.025 * x[2] * (d_init - 0.05 + 0.02 * x[0]))) / rhow_init),
+        lambda x: -(beta0[2] + beta4[2] * Args.fc_jack / Args.fc_init + beta5[2] * Args.fy_jack / Args.fy_core + beta1[2] * (
+                Args.d_init + 0.02 * 2 * x[0]) / Args.d_init + beta2[2] * (
+                            Alunit_jack * x[1] / (Args.d_init + 0.02 * 2 * x[0]) ** 2 / Args.rhol_init) + beta3[2] * (
+                            4 * Asunit_jack / (0.025 * x[2] * (Args.d_init - 0.05 + 0.02 * x[0]))) / Args.rhow_init) /
+                  (beta0[0] + beta4[0] * Args.fc_jack / Args.fc_init + beta5[0] * Args.fy_jack / Args.fy_core + beta1[0] * (
+                          Args.d_init + 0.02 * 2 * x[0]) / Args.d_init + beta2[0] * (
+                           Alunit_jack * x[1] / (Args.d_init + 0.02 * 2 * x[0]) ** 2 / Args.rhol_init) + beta3[0] * (
+                           4 * Asunit_jack / (0.025 * x[2] * (Args.d_init - 0.05 + 0.02 * x[0]))) / Args.rhow_init),
 
         #lambda x: -(beta0[3] + beta4[3] * fc_jack / fc_init + beta5[3] * fy_jack / fy_core + beta1[3] * (
         #        d_init + 0.02 * 2 * x[0]) / d_init + beta2[3] * (Alunit_jack * x[1] / (1.1025 * d_init ** 2)) / (
@@ -222,13 +259,13 @@ def JacketObj():
         #                          Alunit_jack * x[1] / (1.1025 * d_init ** 2)) / (rhol_init) + beta3[0] * (
         #                          4 * Asunit_jack / (
         #                          0.025 * x[2] * (d_init - 0.05 + 0.02 * x[0]))) / rhow_init),
-        lambda x: (co2[0] * 3.14159 / 4 * ((d_init + 2 * 0.02 * x[0]) * (d_init - 2 * 0.02 * x[0])) * 2500 + co2[
+        lambda x: (Args.co2[0] * 3.14159 / 4 * ((Args.d_init + 2 * 0.02 * x[0]) * (Args.d_init - 2 * 0.02 * x[0])) * 2500 + Args.co2[
             1] * Alunit_jack * x[1] * 7850) / 1000]
 
-    constraint = lambda x: -(beta0[4] + beta4[4] * fc_jack / fc_init + beta5[4] * fy_jack / fy_core + beta1[4] * (
-            d_init + 0.02 * 2 * x[0]) / d_init + beta2[4] * (Alunit_jack * x[1] / (1.1025 * d_init ** 2)) / rhol_init +
+    constraint = lambda x: -(beta0[4] + beta4[4] * Args.fc_jack / Args.fc_init + beta5[4] * Args.fy_jack / Args.fy_core + beta1[4] * (
+            Args.d_init + 0.02 * 2 * x[0]) / Args.d_init + beta2[4] * (Alunit_jack * x[1] / (1.1025 * Args.d_init ** 2)) / Args.rhol_init +
                              beta3[4] * (4 * Asunit_jack / (
-                    0.025 * x[2] * (d_init - 0.05 + 0.02 * x[0]))) / rhow_init) + goal2
+                    0.025 * x[2] * (Args.d_init - 0.05 + 0.02 * x[0]))) / Args.rhow_init) + goal2
 
     if callable(constraint):
         constraint = [constraint]
@@ -253,7 +290,6 @@ def JacketObj():
         plot_petal = Petal(bounds=[-2, 2])
         plot_petal.add(res.F[i])
         plot_petal.save(f'PetalPics/petal_population_{i}.png')
-
     utilities.create_population_video()
 
 def FRPdefBounds():
@@ -265,21 +301,10 @@ def FRPdefBounds():
     ub   = [ubx1,ubx2]
     return lb, ub
 
-def FRPdefObj():
+def FRPdefObj(Args):
     intVars=[1,2]
     nvars=2
     lb,ub = FRPdefBounds()
-    ######################################################
-    fc_init = 24
-    Ecore = 29
-    d_init = 1.6
-    t_frp=[0.169/1000, 0.333/1000]
-    Efrp=[210, 225]
-    fjfrp=[3000, 3500]
-    rhow_init =0.0075
-    cost=[400, 600]
-    co2= [0.12,0.684,5]
-    ######################################################
     goal=1.10
     goal2=0.0
     beta0 = [-1.27e+02,-7.610,9.02e+01,7.56e+01,-0.494]
@@ -288,18 +313,18 @@ def FRPdefObj():
     beta3 = [4.06e-03,4.49e-04,1.40e-02,1.11e-02,0.000802]
     beta4 = [4.89e-02,2.91e-02,4.71e-01,4.02e-01,0.0310]
     obj = [
-        lambda x: (cost[0]*d_init*math.pi*1.1*x[1]*(1-x[0])+cost[1]*d_init*math.pi*1.1*x[0]*(x[1]))/1000,
-        lambda x: -(beta0[1] + beta1[1]*(d_init+2*(x[1]*t_frp[1]*(1-x[0])+(x[1])*t_frp[0]*x[0]))/d_init + beta2[1]*(x[0]*Efrp[1]+(1-x[0])*Efrp[0])/Ecore +beta3[1]*(x[0]*fjfrp[1]+(1-x[0])*fjfrp[0])/fc_init +beta4[1]*(4*(x[1]*t_frp[1]*(1-x[0])+(x[1])*t_frp[0]*x[0])/d_init)/rhow_init),
-        lambda x: -(beta0[2] + beta1[2]*(d_init+2*(x[1]*t_frp[1]*(1-x[0])+(x[1])*t_frp[0]*x[0]))/d_init + beta2[1]*(x[0]*Efrp[1]+(1-x[0])*Efrp[0])/Ecore +beta3[1]*(x[0]*fjfrp[1]+(1-x[0])*fjfrp[0])/fc_init +beta4[1]*(4*(x[1]*t_frp[1]*(1-x[0])+(x[1])*t_frp[0]*x[0])/d_init)/rhow_init),
-        lambda x: (co2[2]*(3.14159*(d_init)*1.1*x[1]*(1-x[0])+2*3.14159*(d_init)*1.1*x[1]*(x[0])))*0.304/0.25/1000]
-    constraint = lambda x: -((beta0[3] + beta1[3]*(d_init+2*(x[1]*t_frp[1]*(1-x[0])+(x[1])*t_frp[0]*x[0]))/d_init + beta2[3]*(x[0]*Efrp[1]+(1-x[0])*Efrp[3])/Ecore +beta3[3]*(x[0]*fjfrp[1]+(1-x[0])*fjfrp[0])/fc_init +beta4[3]*(4*(x[1]*t_frp[1]*(1-x[0])+(x[1])*t_frp[0]*x[0])/d_init)/rhow_init))+goal
+        lambda x: (Args.cost[0]*Args.d_init*math.pi*1.1*x[1]*(1-x[0])+Args.cost[1]*Args.d_init*math.pi*1.1*x[0]*(x[1]))/1000,
+        lambda x: -(beta0[1] + beta1[1]*(Args.d_init+2*(x[1]*Args.t_frp[1]*(1-x[0])+(x[1])*Args.t_frp[0]*x[0]))/Args.d_init + beta2[1]*(x[0]*Args.Efrp[1]+(1-x[0])*Args.Efrp[0])/Args.Ecore +beta3[1]*(x[0]*Args.fjfrp[1]+(1-x[0])*Args.fjfrp[0])/Args.fc_init +beta4[1]*(4*(x[1]*Args.t_frp[1]*(1-x[0])+(x[1])*Args.t_frp[0]*x[0])/Args.d_init)/Args.rhow_init),
+        lambda x: -(beta0[2] + beta1[2]*(Args.d_init+2*(x[1]*Args.t_frp[1]*(1-x[0])+(x[1])*Args.t_frp[0]*x[0]))/Args.d_init + beta2[1]*(x[0]*Args.Efrp[1]+(1-x[0])*Args.Efrp[0])/Args.Ecore +beta3[1]*(x[0]*Args.fjfrp[1]+(1-x[0])*Args.fjfrp[0])/Args.fc_init +beta4[1]*(4*(x[1]*Args.t_frp[1]*(1-x[0])+(x[1])*Args.t_frp[0]*x[0])/Args.d_init)/Args.rhow_init),
+        lambda x: (Args.co2[2]*(3.14159*(Args.d_init)*1.1*x[1]*(1-x[0])+2*3.14159*(Args.d_init)*1.1*x[1]*(x[0])))*0.304/0.25/1000]
+    constraint = lambda x: -((beta0[3] + beta1[3]*(Args.d_init+2*(x[1]*Args.t_frp[1]*(1-x[0])+(x[1])*Args.t_frp[0]*x[0]))/Args.d_init + beta2[3]*(x[0]*Args.Efrp[1]+(1-x[0])*Args.Efrp[3])/Args.Ecore +beta3[3]*(x[0]*Args.fjfrp[1]+(1-x[0])*Args.fjfrp[0])/Args.fc_init +beta4[3]*(4*(x[1]*Args.t_frp[1]*(1-x[0])+(x[1])*Args.t_frp[0]*x[0])/Args.d_init)/Args.rhow_init))+goal
     if callable(constraint):
         constraint = [constraint]
     problem = FunctionalProblem(3, obj, constr_ieq=constraint, xl=lb, xu=ub)
 
     method = get_algorithm('nsga2', pop_size=1000)
     res = minimize(problem, method, save_history=False)
-    bwm = best_value(res.X, res.F, 'FRP')
+    bwm = best_value(res.X, res.F, Args.w)
     m = min(bwm)
     print(m)
     plot = Scatter()
@@ -319,15 +344,7 @@ def FRPdefObj():
 
     utilities.create_population_video()
 
-def best_value(x, obj, type):
-    if type == 'FRP':
-        ######################################################
-        w=[0.56, 0.052, 0.164, 0.2196]
-        ######################################################
-    elif type == 'Jacket':
-        ######################################################
-        w = [0.45, 0.045, 0.23, 0.17, 0.1]
-        ######################################################
+def best_value(x, obj, w):
     bwm = np.zeros(len(x))
     for i in range(len(bwm)):
         bwm[i] = sum(x * y for x, y in zip(w, obj[i]))
@@ -338,6 +355,6 @@ def best_value(x, obj, type):
 if __name__ == '__main__':
     run_scenario = '' #Choose Between 'FRP' or 'RCJacket'
     if run_scenario == 'RCJacket':
-        JacketObj()
+        JacketObj(RCJacketInput)
     elif run_scenario == 'FRP':
-        FRPdefObj()
+        FRPdefObj(FRPInput)
